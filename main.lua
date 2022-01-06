b00e9a310e1027731e384ad0a25e48a1159abe05
lolkaska123 <62012705+lolkaska123@users.noreply.github.com>
2022-01-06 19:14:51 +0200

    main.lua
    sdfsdfsdf

---
+++ main.lua
@@ -0,0 +1,6 @@
+
+game:GetService("Workspace").MyNameOTT123.Humanoid.WalkSpeed = 50
+
+game:GetService("Workspace").MyNameOTT123.Humanoid.MaxHealth = 1000
+
+game:GetService("Workspace").MyNameOTT123.Humanoid.Health = 1000
